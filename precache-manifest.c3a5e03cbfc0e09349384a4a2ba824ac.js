cfc5c837310c7067a18e9cb2e10a0c6f47334b28
MikaCinc <mihajlo.ls00@outlook.com>
2020-08-22 15:50:59 +0200

    Updates

---
+++ precache-manifest.c3a5e03cbfc0e09349384a4a2ba824ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec3109add5426560abb689d6db91220",
+    "url": "/package-picker/index.html"
+  },
+  {
+    "revision": "9c0e24e5eca99dd3e5cd",
+    "url": "/package-picker/static/css/main.3fdc0d24.chunk.css"
+  },
+  {
+    "revision": "fd8a9b33be27a02c7911",
+    "url": "/package-picker/static/js/2.23739f9f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/package-picker/static/js/2.23739f9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c0e24e5eca99dd3e5cd",
+    "url": "/package-picker/static/js/main.def2faff.chunk.js"
+  },
+  {
+    "revision": "8d81e30c92fde8bb857b",
+    "url": "/package-picker/static/js/runtime-main.c83dbfd3.js"
+  }
+]);
